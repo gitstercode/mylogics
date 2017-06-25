0a2d3d2c8e2bf87d4e4cee358ce5c0c0ff0d8cd6
Varun <gitstercode@users.noreply.github.com>
2017-06-25 13:24:33 -0700

    Create custom_logger.py

---
+++ custom_logger.py
@@ -0,0 +1,25 @@
+class CustomFormatter(Formatter):
+    def __init__(self):
+        super(CustomFormatter, self).__init__()
+
+    def format(self, record):
+        date_string = datetime.now().strftime("%d/%b/%Y %H:%M:%S")
+        app_id = "DEP"
+        level = record.levelname
+        server = record.args[0]
+        clientip = record.args[1]
+        message = record.msg
+        log_messeage = "[%s] APPID=%s TYPE=%s SERVER=%s CLIENT_IP=%s MESSAGE=%s" % (
+            date_string, app_id, level, server, clientip, message)
+
+        return log_messeage
+        
+ def main():
+     handler = FileHandler('foo.log')
+     handler.setLevel(logging.INFO)
+     handler.setFormatter(CustomFormatter())
+     app.logger.addHandler(handler)
+     
+ server = request.host
+ client_ip=request.remote_addr
+ app.logger.error('An error occurred', server, client_ip)
