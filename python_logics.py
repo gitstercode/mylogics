ea5237ac17f8d5e3af5249e689527257fe432f9a
Varun <gitstercode@users.noreply.github.com>
2017-06-25 12:39:39 -0700

    logic-2 to read and write into json file if json module is not supported.

--- python_logics.py
+++ python_logics.py
@@ -1,3 +1,5 @@
+#####
+## Logic- 1
 ## If a new file name is given as input 
 ## Since the file is not yet created in OS. To make perfect validations, the below code is required.
 
@@ -16,3 +18,43 @@ o = os.path.abspath(o)                  # Gets Absolute path if given path is re
             else:                       # If only a filename is given
                 output = os.getcwd()
                 fileNameOut = o
+#####                
+
+#####
+## Logic -2
+## If the Given json file throws error or if any python library(click) not accepting JSON calls
+## Below Class can help in converting JSON to dict and then writing back to file
+
+class Config(object):
+    def __init__(self):
+        self.contents = None
+        if os.path.exists(CONFIG_FILE):
+            with open(CONFIG_FILE, 'r') as f:
+                s = f.read()
+                self.contents = ast.literal_eval(s)
+
+    def get_config(self):
+        return self.contents
+
+    def write(self, key, value):
+        self.contents[key] = value
+        with open(CONFIG_FILE, 'w') as f:
+            f.write(str(self.contents))
+
+    def remove(self, *args):
+        for key in args:
+            value = self.contents.get(key)
+            if value:
+                self.contents[key] = ""
+                with open(CONFIG_FILE, 'w') as f:
+                    f.write(str(self.contents))
+config = Config()
+some_key = config.get_config().get("some_key")
+config.write("some_key1", "some_value1")
+config.remove("some_key1", "some_key")
+
+#####
+
+#####
+## Logic - 3
+##
